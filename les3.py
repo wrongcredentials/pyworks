7e7e910d146e0fc72a7d94698b6b8b1bbcf7ed22
Илья Алексеев <iaalekseev@sbn-mac-92.local>
2017-05-12 16:59:49 +0300

    Lecture

--- les3.py
+++ les3.py
@@ -3,11 +3,39 @@ class Person:
 		self.name = "vasya"
 		self.age = 28
 
+	def __str__(self):
+		return "Name: {}, Age: {}".format(self.name, self.age)
+
 	def get_name(self):
 		return self.name
+
+	def set_name(self):
+		self.name = name
 
 	def get_age(self):
 		return self.age
 
 p = Person()
 p.get_name()
+
+p2 = Person()
+p2.set_name("petya")
+p2.get.name()
+
+
+class Person2:
+	def __init__(self, name, age):
+		self.name = name
+		self.age = age
+
+	def get_name(self):
+		return self.name
+
+	def set_name(self):
+		self.name = name
+
+	def get_age(self):
+		return self.age
+
+p = Person2()
+p.get_name("vasya", 28)
